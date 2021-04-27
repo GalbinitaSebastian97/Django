eb61c9a82cf6ba70b1771efa25e849ba812733d6
GalbinitaSebastian97 <galbinita.sebastian@yahoo.com>
2021-04-27 23:50:01 +0300

    Created objects for databse, displayed an object on a page

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Products
 # Create your views here.
+
+def product_detail_view(request):
+    obj = Products.objects.get( id = 1)
+    my_context = {
+        "title" : obj.title,
+        "description": obj.description
+    }
+    return render(request, "product/detail.html",my_context)
