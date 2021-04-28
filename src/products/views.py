2b159ea486bfabb9d593bbbca63eda07eec95368
GalbinitaSebastian97 <galbinita.sebastian@yahoo.com>
2021-04-28 11:53:51 +0300

    Successfully added an object to the databse and displayed it on a page

--- src/products/views.py
+++ src/products/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from .models import Products
+from .forms import ProductForm
 # Create your views here.
+
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    my_context = {
+        'form':form
+    }
+    return render(request, "products/product_create.html",my_context)
 
 def product_detail_view(request):
     obj = Products.objects.get( id = 1)
     my_context = {
-        "title" : obj.title,
-        "description": obj.description
+        "object": obj
     }
-    return render(request, "product/detail.html",my_context)
+    return render(request, "products/product_detail.html",my_context)
